451457909e2d32a2b47f14f89be31d30802a956f
Yama Roni <yamaroni8@gmail.com>
2025-05-28 20:17:55 +0700

    update js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Function to display an alert when the button is clicked
+function validateInput() {
+    const inputSuhu = document.getElementById('input-suhu');
+
+    // Validasi input suhu
+    if (inputSuhu.value == '') {
+        alert('Input suhu tidak boleh kosong!');
+    } else {
+        /// Convert input to a number
+    }
+}
